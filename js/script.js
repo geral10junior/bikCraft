8ba9ebe84ac8a1569b6f221eb1daaf630c2ab266
Geraldez Ribeiro <geraldezjunior09@gmail.com>
2025-07-07 14:44:23 -0300

    plugin adicionado

--- js/script.js
+++ js/script.js
@@ -53,7 +53,6 @@ const galeriaContainer = document.querySelector(".bicicleta-imagens");
 function trocarImagem(event) {
   const img = event.currentTarget;
   const media = matchMedia("(min-width: 1000px").matches;
-  console.log(media);
 
   if (media) {
     galeriaContainer.prepend(img);
@@ -65,3 +64,8 @@ function eventosGaleria(img) {
 }
 
 galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
